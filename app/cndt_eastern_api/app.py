990eb7916d6bfb40795f6e7518904991cb09399d
keisukesakasai <k6s4i5.3rx@gmail.com>
2023-11-25 10:56:47 +0900

    :sparkles: Add Auto instrument to Code

--- app/cndt_eastern_api/app.py
+++ app/cndt_eastern_api/app.py
@@ -3,8 +3,44 @@ from flask import Flask, request
 from database import get_population_from_cache, set_population_to_cache, get_population_from_db
 from logger import setup_logger
 
-app = Flask(__name__)
 logger = setup_logger()
+
+# === OTel Configuration.
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+    SimpleSpanProcessor,
+)
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.trace import get_tracer_provider, set_tracer_provider
+
+resource = Resource(attributes={
+    "service.name": "CNDT-EASTER-API"
+})
+
+tracer_provider = TracerProvider(resource=resource)
+tracer = trace.get_tracer(__name__)
+
+otlp_exporter = OTLPSpanExporter(endpoint="http://localhost:4317", insecure=True)
+span_processor = BatchSpanProcessor(otlp_exporter)
+tracer_provider.add_span_processor(span_processor=BatchSpanProcessor(span_exporter=otlp_exporter))
+tracer_provider.add_span_processor(span_processor=SimpleSpanProcessor(span_exporter=ConsoleSpanExporter()))
+trace.set_tracer_provider(tracer_provider)
+
+# === Auto Instrument
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.pymemcache import PymemcacheInstrumentor
+from opentelemetry.instrumentation.mysql import MySQLInstrumentor
+
+instrumentor = FlaskInstrumentor()
+app = Flask(__name__)
+instrumentor.instrument_app(app)
+
+PymemcacheInstrumentor().instrument()
+MySQLInstrumentor().instrument()
 
 # CNDT EASTERN API Main Hander: From Prefecture Name To Population.
 @app.route('/call_eastern_api', methods=['GET'])
