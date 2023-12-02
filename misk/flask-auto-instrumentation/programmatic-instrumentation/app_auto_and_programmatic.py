f29d11f656e6f14e032fb19f59159738f228a335
keisukesakasai <k6s4i5.3rx@gmail.com>
2023-12-02 16:34:21 +0900

    :sparkles: ちょっと検証した（完全自動 + Programmatic について）

---
+++ misk/flask-auto-instrumentation/programmatic-instrumentation/app_auto_and_programmatic.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    BatchSpanProcessor,
+    ConsoleSpanExporter,
+)
+from opentelemetry.trace import get_tracer_provider, set_tracer_provider
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+
+from opentelemetry import trace
+tracer = trace.get_tracer(__name__)
+
+url_traces = 'http://localhost:4317'
+otlp_exporter_traces = OTLPSpanExporter(endpoint=url_traces)
+# tracer.add_span_processor(span_processor=BatchSpanProcessor(span_exporter=otlp_exporter_traces))
+
+def request_hook(span, environ):
+    if span and span.is_recording():
+        span.set_attribute("custom_user_attribute_from_request_hook", "some-value")
+
+def response_hook(span, status, resresponse_header):
+    if span and span.is_recording():
+        span.set_attribute("custom_user_attribute_from_response_hook", "some-value")
+
+app = Flask(__name__)
+FlaskInstrumentor().instrument_app(
+    app=app,
+    request_hook=request_hook, 
+    response_hook=response_hook
+)
+    
+print(app)
+# instrumentor.instrument_app(app, excluded_urls="/server_request")
+
+@app.route("/server_request")
+def server_request():
+    print(request.args.get("param"))
+    return "served"
+
+if __name__ == "__main__":
+    app.run(port=8082)
